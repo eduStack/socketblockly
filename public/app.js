f4aec7282c90cdd73d2a54d65232daa152af6017
ucanbaklava <ucanbaklava@outlook.com>
2017-04-09 12:54:36 +0200

    changes

--- public/app.js
+++ public/app.js
@@ -18191,6 +18191,13 @@ var workspaceOptions = {
 	grid: {
 		spacing: 10
 	},
+  zoom:
+              {controls: true,
+               wheel: true,
+               startScale: 1.0,
+               maxScale: 3,
+               minScale: 0.3,
+               scaleSpeed: 1.2},
 	toolbox: $toolbox,
 	trashcan: true
 }
